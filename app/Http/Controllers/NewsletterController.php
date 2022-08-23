11fcf6304a0a73d67a488d01034304ef171cff2f
shivam <shivamyadav4046@gmail.com>
2022-08-23 10:01:27 +0530

    hosting

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Services\Newsletter;
+use Exception;
+// use Newsletter;
+use Illuminate\Validation\ValidationException;
+
+class NewsletterController extends Controller
+{
+    public function __invoke(Newsletter $newsletter)
+    {
+        request()->validate(['email' => 'required|email']);
+
+        try {
+            $newsletter->subscribe(request('email'));
+        } catch (Exception $e) {
+            // dd($e);
+            throw ValidationException::withMessages([
+                'email' => 'This email could not be added to our newsletter list.'
+            ]);
+        }
+
+        return redirect('/')
+            ->with('success', 'You are now signed up for our newsletter!');
+    }
+}
